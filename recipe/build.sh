17793c678d77cc3e5022245cc808f4140da27c61
Uwe L. Korn <uwe.korn@quantco.com>
2024-09-27 10:09:09 +0200

    yarn install

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,12 +7,13 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
 fi
 # Don't use pre-built gyp packages
 export npm_config_build_from_source=true
+export NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
 rm $PREFIX/bin/node
 ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
 
+yarn install --frozen-lockfile
 yarn pack
 yarn licenses generate-disclaimer > ThirdPartyLicenses.txt
-NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
 npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
